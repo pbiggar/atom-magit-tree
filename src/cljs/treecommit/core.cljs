8342ada5dcce80928cabacbfa52eb1de951af266
Paul Biggar <paul.biggar@gmail.com>
2016-08-13 21:28:40 -0700

    Get movement working

--- src/cljs/treecommit/core.cljs
+++ src/cljs/treecommit/core.cljs
@@ -18,9 +18,6 @@
 (defn firstA [fake-array]
   (aget fake-array 0))
 
-(defn in-treeview? [e]
-  (-> e .-target .-classList firstA (= "tree-view")))
-
 ;; TODO: it won't always be the first repo
 ;;  for projectPath, i in atom.project.getPaths()
 ;;     if goalPath is projectPath or goalPath.indexOf(projectPath + path.sep) is 0
@@ -28,9 +25,11 @@
 (defn get-repo []
   (-> js/atom .-project .getRepositories firstA))
 
-(defn get-path []
-  (-> (active-editor)
-      .getPath))
+(defn get-selected-path []
+  (-> js/atom
+      .-document
+      (.querySelector ".tree-view .selected [data-path]")
+      (.getAttribute "data-path")))
 
 (defn callback [op file]
   (fn [err stdout stderr]
@@ -40,55 +39,73 @@
       (println (str "stderr was: " stderr)))))
 
 
+;; TODO: there must be a better way to get this.
+(defn get-treeview-target []
+  (-> js/atom
+      .-document
+      (.querySelector "ol.tree-view")))
+
+
+(defn dispatch-treeview-command [command]
+  (let [target (get-treeview-target)]
+    (-> js/atom
+        .-commands
+        (.dispatch target command))))
+
+(defn get-gifted []
+  (gift (-> (get-repo) .-path (str/strip-suffix "/.git"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; commands
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn stage []
-  (let [repo (get-repo)
-        gifted (gift (-> repo .-path (str/strip-suffix "/.git")))
-        path (get-path)]
 
+(defn stage []
+  (let [gifted (get-gifted)
+        path (get-selected-path)]
     (.add gifted #js[path]
           (callback "stage" path))))
 
 (defn unstage []
-  (let [repo (get-repo)
-        gifted (gift (-> repo .-path (str/strip-suffix "/.git")))
-        path (get-path)]
-
+  (let [gifted (get-gifted)
+        path (get-selected-path)]
     (.reset gifted (str "HEAD " #js[path])
             (callback "unstage" path))))
 
+(defn move-up []
+  (dispatch-treeview-command "core:move-up"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; disposables and event handlers
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn move-down []
+  (dispatch-treeview-command "core:move-down"))
 
-(def CompositeDisposable (-> "atom" nodejs/require .-CompositeDisposable))
-(def disposables (CompositeDisposable.))
-
-(defn register-commands []
-  (.add disposables
-        (.add js/atom.commands "atom-workspace" "treecommit:stage-file", stage)
-        (.add js/atom.commands "atom-workspace" "treecommit:unstage-file", unstage)))
-
-
-(defn free-disposibles []
-  (.dispose disposables))
-
+(defn open-selected []
+  (dispatch-treeview-command "tree-view:open-selected-entry"))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Initialization and deinitialization
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(def CompositeDisposable (-> "atom" nodejs/require .-CompositeDisposable))
+(def disposables (CompositeDisposable.))
+
+(defn register-commands []
+  (.add disposables
+        (.add js/atom.commands "atom-workspace" "treecommit:stage-selected", stage)
+        (.add js/atom.commands "atom-workspace" "treecommit:unstage-selected", unstage)
+        (.add js/atom.commands "atom-workspace" "treecommit:move-treeview-up", move-up)
+        (.add js/atom.commands "atom-workspace" "treecommit:move-treeview-down", move-down)
+        (.add js/atom.commands "atom-workspace" "treecommit:open-treeview-selected", open-selected)))
+
+
+(defn free-disposibles []
+  (.dispose disposables))
+
+
 (defn activate [state]
   (set! js/saved nil)
   (register-commands))
 
 (defn deactivate []
-  (println "Deactivating from treecommit")
   (free-disposibles))
 
 (defn serialize [])
